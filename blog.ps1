80f235110e9b660082711f24c9f73f75fdcb94ed
Sergey Kanzhelev <S.Kanzhelev@live.com>
2020-06-27 23:08:40 -0700

    remove octopress

--- blog.ps1
+++
@@ -1,5 +0,0 @@
-C:\src\github\yari\rubyinstaller.ps1 "2.2.3"
-$env:path = $env:path + ";C:\Python27"
-$env:path = $env:path + ";C:\tools\which2.20\bin"
-cd C:\src\github\apmtips
-& 'C:\Program Files\IIS Express\iisexpress.exe' /path:c:\src\github\apmtips\public /port:1211
